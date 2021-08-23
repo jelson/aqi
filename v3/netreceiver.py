32d4d3c74baaf7a3ae2abaefce8ab598339368f6
Jeremy Elson <jelson@gmail.com>
2021-08-22 22:52:09 -0700

    Move netreceiver from python's builtin http.server to cherrypy

--- v3/netreceiver.py
+++ v3/netreceiver.py
@@ -2,32 +2,27 @@
 
 from mylogging import say
 import argparse
+import cherrypy
 import datetime
-import http.server
 import json
 import pms5003db
 import socket
 
 PORT = 15000
 
-class SensorDataHandler(http.server.BaseHTTPRequestHandler):
-    def do_POST(self):
-        len = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(len)
-        msg = json.loads(post_data)
+class SensorDataHandler():
+    def __init__(self):
+        self.db = pms5003db.PMS5003Database()
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def data(self):
+        msg = cherrypy.request.json
         sensorid = msg['sensorid']
         sensordata = msg['sensordata']
         for rec in sensordata:
             rec['time'] = datetime.datetime.fromtimestamp(rec['time'])
-        self.server.db.insert_batch(sensorid, sensordata)
-        self.send_response(200)
-        self.end_headers()
-
-class SensorDataServer(http.server.ThreadingHTTPServer):
-    def __init__(self, db):
-        self.address_family = socket.AF_INET6
-        super(SensorDataServer, self).__init__(("", PORT), SensorDataHandler)
-        self.db = db
+        self.db.insert_batch(sensorid, sensordata)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -39,10 +34,11 @@ def main():
     args = parser.parse_args()
     if args.log:
         logging.open_logfile(args.log)
-
-    db = pms5003db.PMS5003Database()
-    server = SensorDataServer(db)
-    say(f"Starting on port {PORT}")
-    server.serve_forever()
+    server = SensorDataHandler()
+    cherrypy.config.update({
+        'server.socket_host': '::',
+        'server.socket_port': PORT,
+    })
+    cherrypy.quickstart(SensorDataHandler())
 
 main()
