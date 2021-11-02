0eff3af1b5b1c972a4c019adfe201c7a8e7d3d97
Jeremy Elson <jelson@gmail.com>
2021-11-01 23:22:24 -0700

    Add basic integrations framework
    Add basic framework that allows triggering of events based on AQI
    changes.  If AQI goes over a threshold, send a message to Nest
    thermostat that the whole-house HVAC fan should be turned on.

---
+++ v3/integrations/aqi-fan-integration.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+# To trigger, send a dbus message thusly:
+#
+# dbus-send --system --type=signal /org/lectrobox/aqi org.lectrobox.aqi.NewDataAvailable  dict:string:int32:sensorid,1
+#
+
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+import dbus
+import nest_controller
+import os
+import psycopg2
+import sys
+
+# project libraries
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "aqi", "v3"))
+import server.pms5003db as pms5003db
+
+CONFIG = [
+    {
+        'sensorid': 1,
+        'on-thresh': 30,
+        'off-thresh': 5,
+        'averaging-sec': 60,
+        'onoff-func': nest_controller.NestController('Jer Entryway').fan_control,
+    }
+]
+
+class AQIChangeHandler:
+    def __init__(self, bus):
+        bus.add_signal_receiver(
+            self.NewDataAvailable,
+            path='/org/lectrobox/aqi',
+            signal_name='NewDataAvailable',
+        )
+        self.pmsdb = pms5003db.PMS5003Database()
+
+    # dbus signal handler
+    def NewDataAvailable(self, *args, **kwargs):
+        argdict = dict(args[0])
+        sensorid = int(argdict['sensorid'])
+
+        for c in CONFIG:
+            if c['sensorid'] == sensorid:
+                self.maybe_on_off(c)
+
+    def maybe_on_off(self, c):
+        aqi = self.get_oneminute_average(c)
+        print(f"sensor id {c['sensorid']} aqi now {aqi}")
+
+        fan_is_on = c.get('fan-is-on', False)
+
+        if fan_is_on and aqi <= c['off-thresh']:
+            self.change_fan_state(c, False)
+        elif (not fan_is_on) and aqi >= c['on-thresh']:
+            self.change_fan_state(c, True)
+
+    def get_oneminute_average(self, c):
+        cursor = self.pmsdb.get_raw_db().cursor()
+        cursor.execute(
+            'select avg("aqi2.5") from particulatev3 where sensorid=%s and time > now() - interval \'%s seconds\'',
+            (c['sensorid'], c['averaging-sec'])
+        )
+        row = cursor.fetchone()
+        return(row[0])
+
+    def change_fan_state(self, c, onoff):
+        c['fan-is-on'] = onoff
+        print(f'SensorID {c["sensorid"]} tripped over threshold')
+        c['onoff-func'](onoff)
+
+def main():
+    DBusGMainLoop(set_as_default=True)
+    bus = dbus.SystemBus()
+    ch = AQIChangeHandler(bus)
+    loop = GLib.MainLoop()
+    loop.run()
+
+main()
---
+++ v3/integrations/nest_controller.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import pathlib
+import requests
+import yaml
+
+class NestController():
+    def __init__(self, device_name):
+        config_fn = os.path.join(
+            pathlib.Path.home(),
+            ".config", "aqi", "nest-client.yml")
+        config = yaml.safe_load(open(config_fn, "r"))
+        self.config = config['client_config']
+        if not device_name in config['devices']:
+            raise(f"No such device '{device_name}' in config file '{config_fn}'")
+        self.config.update(config['devices'][device_name])
+
+    def _execute_request(self, url, json_body=None):
+        # First, get a token using the refresh token
+        params = {
+            'client_id': self.config['client_id'],
+            'client_secret': self.config['client_secret'],
+            'refresh_token': self.config['refresh_token'],
+            'grant_type': 'refresh_token',
+        }
+        resp = requests.post(self.config['oauth_url'], params=params)
+        if not resp or resp.status_code != 200:
+            raise("Failed to refresh token")
+        token_info = resp.json()
+
+        # Now, execute the request
+        headers = {
+            'Authorization': 'Bearer ' + token_info['access_token'],
+        }
+        print(f"Sending request to {url}: {json_body}")
+        if json_body:
+            resp = requests.post(url, headers=headers, json=json_body)
+        else:
+            resp = requests.get(url, headers=headers)
+
+        json_response = resp.json()
+        print(f"Got response, status {resp.status_code}: {json.dumps(json_response, indent=True)}")
+        return json_response
+
+    def list_all_devices(self):
+        status = self._execute_request("{control_url}/enterprises/{project}/devices".format(**self.config))
+
+    def get_status(self):
+        status = self._execute_request("{control_url}/enterprises/{project}/devices/{device}".format(**self.config))
+
+    def _command_url(self):
+        return "{control_url}/enterprises/{project}/devices/{device}:executeCommand".format(**self.config)
+
+    def fan_control(self, onoff):
+        if onoff:
+            params = {
+                "timerMode": "ON",
+                "duration": "3600s",
+            }
+        else:
+            params = {
+                "timerMode": "OFF",
+            }
+
+        self._execute_request(self._command_url(), json_body = {
+            "command": "sdm.devices.commands.Fan.SetTimer",
+            "params": params
+        })
+
+def main():
+    controller = NestController('Jer Entryway')
+    controller.get_status()
+
+if __name__ == '__main__':
+    main()
